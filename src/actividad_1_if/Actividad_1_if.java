59d7a0957369c6e29d90538fccd3cc5971b67382
salva <salva@LAPTOP-B2KFIIUB>
2021-04-20 20:26:53 -0500

    Actividad 1 if_66136

--- src/actividad_1_if/Actividad_1_if.java
+++ src/actividad_1_if/Actividad_1_if.java
@@ -5,6 +5,8 @@
  */
 package actividad_1_if;
 
+import java.util.Scanner;
+
 /**
  *
  * @author salva
@@ -16,6 +18,24 @@ public class Actividad_1_if {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+Scanner entrada = new Scanner(System.in);
+       
+       System.out.println("Introduce un año");
+       int año = entrada.nextInt();
+       int añot = 0;
+       
+       if(año < 2021){
+           añot = 2021 - año;
+           System.out.println("Han pasado " + añot + " años");
+       } else if(año > 2021){
+           añot = año - 2021;
+           System.out.println("Faltan " + añot + " años");
+       } else{
+           System.out.println("Estamos en el mismo año");
+       }
+       
     }
-    
+
 }
+
+     
